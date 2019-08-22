49917a396d3dee91af2a055be10a7cd525bb7416
gabrielk <mail@gabrielkovacs.net>
2019-08-22 17:26:57 +0200

    added the submit price change form

--- cocoFront/src/app/uc7changeprice/uc7view/uc7view.component.ts
+++ cocoFront/src/app/uc7changeprice/uc7view/uc7view.component.ts
@@ -10,22 +10,46 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class Uc7viewComponent implements OnInit {
   
+  cifra : number;
   text : String;
   stockItem : IStockItem[];
   storeIdForm : FormGroup;
+  changePriceForm : FormGroup;
   storeId : number;
+  
 
   constructor(private uc7service: Uc7serviceService) {
     this.text = uc7service.getMessage();
+    this.cifra = 665;
   }
 
   ngOnInit() {
     this.storeIdForm = new FormGroup({
       'storeId' : new FormControl(null)
     });
+
+    this.changePriceForm = new FormGroup({
+      'stockItemId' : new FormControl(null),
+      'stockItemAmount' : new FormControl(null),
+      'stockIdMinStock' : new FormControl(null),
+      'stockIdMaxStock' : new FormControl(null),
+      'stockIdSalePrice' : new FormControl(null),
+      'stockIdProductId' : new FormControl(null),
+      'stockIdStoreId' : new FormControl(null),
+
+    });
   }
   
-  
+  setChangePriceStockItemObject(si : IStockItem){
+    this.changePriceForm.get("stockItemId").setValue(si.id);
+    this.changePriceForm.get("stockItemAmount").setValue(si.amount);
+    this.changePriceForm.get("stockIdMinStock").setValue(si.minStock);
+    this.changePriceForm.get("stockIdMaxStock").setValue(si.maxStock);
+    this.changePriceForm.get("stockIdProductId").setValue(si.productId);
+    this.changePriceForm.get("stockIdStoreId").setValue(si.storeId);
+    
+
+  }
 
   getStockItems(){
     this.storeId = this.storeIdForm.value.storeId
@@ -33,7 +57,11 @@ export class Uc7viewComponent implements OnInit {
   }
 
   onChangePrice(si:IStockItem){
+    this.setChangePriceStockItemObject(si);
     console.log("Selected stock item id: ", si.id );
   }
 
+  setStockItemPrice(){
+    console.log("Stock Item ID: ", this.changePriceForm.value);
+  }
 }
